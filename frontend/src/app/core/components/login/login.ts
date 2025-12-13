52a0b3e1bdf44707410eafd84ce06bfcfd689452
PrinceDev <davinomoises@outlook.com>
2025-12-12 22:01:23 -0400

    feat: add login

---
+++ frontend/src/app/core/components/login/login.ts
@@ -0,0 +1,23 @@
+import { Component,inject  } from '@angular/core';
+import {LoginService} from '../../services/login-service';
+import {Router} from '@angular/router';
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+ private servico = inject(LoginService);
+ private rota = inject(Router);
+ Login(username: string, password: string) {
+    const user = this.servico.Login(username, password);
+    if (user!=null) {
+          this.rota.navigate(['home']);
+    } else {
+      console.log("Usuário ou senha inválidos");
+      alert("Usuário ou senha inválidos");
+    }
+ }
+}
+
