25036920b639916093255f7715d223d5a1b84aa4
Allan Resutto <resutto@izycon.com.br>
2025-12-20 00:46:14 -0400

    opcoes de menu agrupados por Grupo de Usuario

--- frontend/src/app/pages/home/admin/admin/admin.ts
+++ frontend/src/app/pages/home/admin/admin/admin.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
